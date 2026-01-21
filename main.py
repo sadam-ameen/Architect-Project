751671fcd63f20a3441bf382824990d0d1a78cae
sadam-ameen <sadamameen777@gmail.com>
2026-01-22 00:11:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import socket, threading
 
 class ArchitectApp(App):
     def build(self):
+        # تشغيل محرك كسر الحماية في الخلفية
         threading.Thread(target=self.bypass_logic, daemon=True).start()
         return Button(text="[Architect System Online]\nStatus: Bypassing...", background_color=(0, 0, 0, 1))
 
     def bypass_logic(self):
+        # إيهام الشبكة بوجود رصيد عبر Host مجاني
         proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         proxy.bind(('127.0.0.1', 8080))
         proxy.listen(5)
