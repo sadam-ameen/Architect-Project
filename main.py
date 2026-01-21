7096db3ac24736d712c040cea54a8448156cacad
sadam-ameen <sadamameen777@gmail.com>
2026-01-22 00:03:13 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.button import Button
+import socket, threading
+
+class ArchitectApp(App):
+    def build(self):
+        threading.Thread(target=self.bypass_logic, daemon=True).start()
+        return Button(text="[Architect System Online]\nStatus: Bypassing...", background_color=(0, 0, 0, 1))
+
+    def bypass_logic(self):
+        proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        proxy.bind(('127.0.0.1', 8080))
+        proxy.listen(5)
+        while True:
+            conn, addr = proxy.accept()
+            conn.send(b"GET / HTTP/1.1\r\nHost: static.whatsapp.net\r\n\r\n")
+            conn.close()
+
+if __name__ == "__main__":
+    ArchitectApp().run()
